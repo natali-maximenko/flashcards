23e5b0d732ceb9b1a96b9323d47ecf03e79b8ddc
Natali Maximenko <natali.maximenko@gmail.com>
2017-06-03 16:45:28 +0300

    find_card before_action in card controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,6 @@
 class CardsController < ApplicationController
+  before_action :find_card, only: [:edit, :update, :show, :destroy]
+
   def index
     @cards = Card.all
   end
@@ -18,12 +20,9 @@ class CardsController < ApplicationController
   end
 
   def edit
-    @card = Card.find(params[:id])
   end
 
   def update
-    @card = Card.find(params[:id])
-
     if @card.update(card_params)
       redirect_to cards_url
     else
@@ -32,11 +31,9 @@ class CardsController < ApplicationController
   end
 
   def show
-    @card = Card.find(params[:id])
   end
 
   def destroy
-    @card = Card.find(params[:id])
     @card.destroy
 
     redirect_to cards_path
@@ -45,5 +42,9 @@ class CardsController < ApplicationController
 private
   def card_params
     params.require(:card).permit(:original_text, :translated_text, :review_date)
+  end
+
+  def find_card
+    @card = Card.find(params[:id])
   end
 end
