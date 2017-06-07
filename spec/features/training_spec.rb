f6e0f6779febfa5369940577c5b782a100a93ed1
Natali Maximenko <natali.maximenko@gmail.com>
2017-06-07 17:53:05 +0300

    fix training spec

--- spec/features/training_spec.rb
+++ spec/features/training_spec.rb
@@ -1,35 +1,39 @@
 require 'rails_helper'
 
 describe 'card review page' do
-  before do
-    card = create(:card)
-    card.review_date = 3.days.ago
+  let!(:card) do
+    card = create(:haben_card)
+    card.review_date = 7.days.ago
     card.save
-    visit root_path
+    card
+  end
+  let!(:haben_card) do
+     c = create(:haben_card)
+     c.review_date = 3.days.ago
+     c.save
+     c
+  end
+  before :each do
+    check_card(text)
   end
 
   context 'see home page with training program' do
+    let(:text) { '' }
     it { expect(page).to have_content 'Первый в мире удобный менеджер флеш-карточек' }
   end
 
   context 'have cards to review' do
+    let(:text) { '' }
     it { expect(page).not_to have_content 'Нечего повторять!' }
   end
 
-  describe 'check card' do
-    before do
-      fill_in :user_text, with: text
-      click_button 'Проверить!'
-    end
+  context 'wrong word' do
+    let(:text) { 'blabla' }
+    it { expect(page).to have_content 'Ошибочка вышла, попробуй ещё раз.' }
+  end
 
-    context 'correct word' do
-      let(:text) { Card.find(find_by_id('id', visible: :all).value).original_text }
-      it { expect(page).to have_content 'Верно! Продолжай.' }
-    end
-
-    context 'wrong word' do
-      let(:text) { 'blabla' }
-      it { expect(page).to have_content 'Ошибочка вышла, попробуй ещё раз.' }
-    end
+  context 'correct word' do
+    let(:text) { 'haben' }
+    it { expect(page).to have_content 'Верно! Продолжай.' }
   end
 end
