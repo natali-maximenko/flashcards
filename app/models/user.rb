ded6fbc1c0819881665402628216db6d840f2545
Natali Maximenko <natali.maximenko@gmail.com>
2017-06-09 16:33:15 +0300

    fix user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,3 @@
-class EmailValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)`[a-z]{2,})\z/i
-      record.errors[attribute] << (options[:message] || "is not an email")
-    end
-  end
-end
-
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, :password, presence: true
