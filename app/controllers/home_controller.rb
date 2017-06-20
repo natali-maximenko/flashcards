5b3494e755a98a4aff99a0060a02df7eaf747afb
Natali Maximenko <natali.maximenko@gmail.com>
2017-06-20 22:33:55 +0300

    code climate

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
     @card = if current_user.current_pack_id.nil?
-      current_user.cards.need_review.random.first
-    else
-      current_user.current_pack.cards.need_review.random.first
-    end
+              current_user.cards.need_review.random.first
+            else
+              current_user.current_pack.cards.need_review.random.first
+            end
   end
 end
